021223a12c914b7157128e0c18254fb3828927d5
Michael Pogoda <do0dlez1991@gmail.com>
2014-12-15 00:50:12 +0200

    Run QML UI in main.

--- main.cxx
+++ main.cxx
@@ -1,25 +1,12 @@
 #include <QApplication>
-#include "mainwindow.h"
-#include <memory>
+#include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
-    QApplication a( argc, argv );
-    std::unique_ptr< virtualBartender::MainWindow > w{ new virtualBartender::MainWindow{} };
+    QApplication app(argc, argv);
 
-    w->show();
-    return a.exec();
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    return app.exec();
 }
-
-//#include <QApplication>
-//#include <QQmlApplicationEngine>
-
-//int main(int argc, char *argv[])
-//{
-//    QApplication app(argc, argv);
-
-//    QQmlApplicationEngine engine;
-//    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-
-//    return app.exec();
-//}
